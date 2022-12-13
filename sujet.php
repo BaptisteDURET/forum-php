fc803afcc421cbfb9af197d7dc93b21770b3f8f8
Baptiste <bluebaptiste982@gmail.com>
2022-12-13 16:20:31 +0100

    balises dans msgs

--- sujet.php
+++ sujet.php
@@ -17,11 +17,43 @@ if (!empty($_POST)){
     ]);
 }
 
+$title = $db-> prepare('SELECT * FROM `sujet`
+                        WHERE Identifiant_Sujet = :id');
+$title->execute([
+    "id" => $_GET['id']
+]);
+$TitleSujet = $title->fetch();
+
+
+if(!empty($_GET['idmessage']) && $_SESSION['admin'] == 1)
+{
+    $idmessage = $_GET['idmessage'];
+    DeleteMessage($idmessage);
+}
+else if(!empty($_GET['idmessage']) && $_SESSION['admin'] == 0)
+{
+
+    echo '<h2 style="color:red; font-size:50px;">Arrête de faire n\'importe quoi !<h2>';
+    header( "refresh:5;url=logout.php" );
+
+    
+}
+
+function DeleteMessage($idmsg)
+{
+    $db = new PDO('mysql:host=localhost;port=3307;dbname=forum-php', 'root');
+    $sqldel = "DELETE FROM message WHERE Identifiant_MSG = :idmsg";
+    $querydel = $db->prepare($sqldel);
+    $querydel->execute([
+        'idmsg' => $idmsg
+    ]);
+    header('Location: sujet.php?id='.$_GET['id']);
+}
 ?>
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="FR-fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -30,14 +62,28 @@ if (!empty($_POST)){
     <title>Topic</title>
 </head>
 <body>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+
+    <script>
+        tinymce.init({
+            selector: 'textarea#commenter',
+            toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ',
+            valid_elements : 'em,strong'
+        });
+    </script>
+
+    <?php
+    echo '<h1>'.$TitleSujet["Libelle"].'</h1>';
+    ?> 
+    
     <div class="formulaire">
         <form method="POST">
-            <textarea name="commenter" id="comment" cols="30" rows="5" placeholder="Message" required="required"></textarea>
+            <textarea name="commenter" id="commenter" cols="30" rows="5" placeholder="Message"></textarea>
             <input type="submit" name="ajouter" value="ajouter" id="ajouter">
         </form>
         <button onclick="rtn()" class="bouton">Retour</button>
         <script>
-            function rtn() {
+            function  rtn() {
                 window.location.href= 'index.php';
             }
         </script>
@@ -46,7 +92,7 @@ if (!empty($_POST)){
         <?php
             $sql2 = $db->prepare('SELECT * FROM `message`
                                 INNER JOIN utilisateur ON message.Identifiant_Utilisateur = utilisateur.Identifiant_Utilisateur
-                                WHERE Identifiant_Sujet = :id');
+                                WHERE Identifiant_Sujet = :id ORDER BY Date_Creation DESC');
             $result = $sql2->execute([
                 "id" => $_GET['id']
             ]);
@@ -55,20 +101,39 @@ if (!empty($_POST)){
                 $name = $row['Nom_utilisateur'];
                 $contenue = $row['Contenue'];
                 $date = $row['Date_Creation'];
-
+                if($row['Image_Profil'] != NULL)
+                {
+                    $ImageProfil = $row['Image_Profil'];
+                }
                 echo '<tr height="50px">';
                     echo '<td>';
+                    if($row['Image_Profil'] != NULL)
+                    {
+                        echo '<img src="'.$ImageProfil.'" alt="Image de profil" width="50px" height="50px">';
+                    }   
                         echo $name;
                     echo '</td>';
                     echo '<td>';
-                        echo htmlspecialchars($contenue);
+                        echo html_entity_decode($contenue);
                     echo '</td>';
                     echo '<td class="date">';
                         echo $date;
                     echo '</td>';
+                    if($_SESSION['admin'] == 1)
+                    {
+                        echo '<td>';
+                            echo '<button style="margin-left: 10px;" onclick="deleteMessage('.$row['Identifiant_MSG'].')">Supprimer</button>';
+                        echo '</td>';
+                    }
                 echo '</tr>';
             };
         ?>
     </table>
 </body>
 </html>
+<script>
+    
+    function deleteMessage(id) {
+        window.location.href=  window.location.href + '&idmessage=' + id;
+    }
+</script>
